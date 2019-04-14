b13beb2ee2dfcc57dceec3ce3524c95069f4393d
Victor Abayomi <victor.abayomi@outlook.com>
2019-04-14 23:45:47 +0100

    Correct text in home route

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('With Banka the views are better'));
+router.get('/', (req, res) => res.send('With Banka the views are better.'));
 
 export default router;
