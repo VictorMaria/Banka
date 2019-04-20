d90b3f93b3e88d7dd9cc9a5ea6b718cf730a9d70
Victor Abayomi <victor.abayomi@outlook.com>
2019-04-20 19:44:49 +0100

    Adjust code

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,21 +1,9 @@
 import express from 'express';
-import accountController from '../controllers/account';
 import newbankAccountDetails from '../validation/newBankAccountDetails';
-import transactionDetails from '../validation/transactionDetails';
 import validator from '../middleware/validator';
-import verifyToken from '../middleware/verifyToken';
-import checkIfAdminStaff from '../helpers/checkIfAdminStaff';
-import checkIfStaff from '../helpers/checkIfStaff';
 
 const router = express.Router();
 
-router.post('/accounts', validator(newbankAccountDetails), accountController.createBankAccount);
-router.get('/accounts/:accountNumber', accountController.findBankAccount);
-router.patch('/accounts/:accountNumber', verifyToken, checkIfAdminStaff, accountController.activateDeactivate);
-router.get('/accounts/:accountNumber/balance', accountController.checkBalance);
-router.post('/transactions/:accountNumber/credit', verifyToken, checkIfStaff, validator(transactionDetails), accountController.creditAccount);
-router.post('/transactions/:accountNumber/debit', verifyToken, checkIfStaff, validator(transactionDetails), accountController.debitAccount);
-router.delete('/accounts/:accountNumber', verifyToken, checkIfAdminStaff, accountController.deleteBankAccount);
-
+router.post('/accounts', validator(newbankAccountDetails));
 
 export default router;
