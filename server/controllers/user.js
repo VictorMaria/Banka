d53bf9583f9a72f39ff553ac935e2a56271380b9
Victor Abayomi <victor.abayomi@outlook.com>
2019-04-22 10:16:25 +0100

    feature(database): create sign in endpoint [Finishes #165490566]

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -34,5 +34,32 @@ class User {
       return res.status(400).send(error);
     }
   }
+
+  static async signIn(req, res) {
+    try {
+      const { rows } = await db.query(userQueries.signInQuery, [req.body.email]);
+      if (!rows[0]) {
+        return res.status(400).send({ status: 400, error: 'Incorrect Credentials' });
+      }
+      if (!helper.comparePassword(rows[0].password, req.body.password)) {
+        return res.status(400).send({ status: 400, error: 'Incorrect Credentials' });
+      }
+      const userToken = helper.generateToken(rows[0].id, rows[0].is_admin, rows[0].is_staff);
+      const response = {
+        status: 200,
+        data: {
+          token: userToken,
+          id: rows[0].id,
+          firstName: rows[0].first_name,
+          lastName: rows[0].last_name,
+          email: rows[0].email,
+          type: rows[0].type,
+        },
+      };
+      return res.status(200).send(response);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
 }
 export default User;
--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const userQueries = {
     users(first_name, last_name, email, password, type, profile_photo)
     values($1, $2, $3, $4, $5, $6)
     returning *`,
+  signInQuery: 'SELECT * FROM users WHERE email = $1',
 };
 
 export default userQueries;
