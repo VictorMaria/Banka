a9db4ffbe3d6f79adc8b9c0290c6c5fdeefa736a
Victor Abayomi <victor.abayomi@outlook.com>
2019-07-03 02:45:14 +0100

    feature(specific-user):consume specific user endpoint [Starts #167005638]

--- UI/JS/one-user.js
+++ UI/JS/one-user.js
@@ -2,6 +2,8 @@ const pageURL = window.location.href;
 const url = new URL(pageURL);
 const id = url.searchParams.get('id');
 const urlToReach = `https://hibanka.herokuapp.com/api/v1/users/${id}`;
+const staffUrl = `${urlToReach}/staff`;
+const adminUrl = `${urlToReach}/admin`;
 const userToken = localStorage.getItem('token');
 const detailsButton = document.getElementById('details');
 const manageButton = document.getElementById('manage');
@@ -50,6 +52,152 @@ const getUserDetails = async () => {
     }
 } 
 
+const manageUserAccount = async () => {
+    displayDiv.innerHTML = `<img src = '../images/fading.gif'>`
+    if (!userToken) {
+        location.href = '../UI/pages/sign-in.html';
+    } else {
+        try {
+            const response = await fetch(urlToReach, { method: 'GET', headers: { 'Content-type': 'application/json',
+                                                                              'x-access-token': userToken  
+                                                                            }});
+            const jsonResponse = await response.json();
+            const userDetails = jsonResponse.data    
+            if (jsonResponse.status === 200){
+                displayDiv.innerHTML = `<div class = 'main-body'>
+                                        <h2>Staff Rights: ${userDetails.isStaff}</h2>
+                                        <button id = 'staff' onclick = 'showStaffModal()'>Update Rights</button><br><br>
+                                        <h2>Admin Rights: ${userDetails.isAdmin}</h2>
+                                        <button id = 'admin' onclick = 'showAdminModal()'>Update Rights</button><br><br>
+                                        </div>
+                                     `;
+            } else if (jsonResponse.status === 404){
+                displayDiv.innerHTML = `<div class = 'main-body'><strong>${jsonResponse.error}</strong></div>`;
+            }  else if (jsonResponse.status === 403) {
+                location.href = '../pages/sign-in.html'
+            }                                                             
+        
+        } catch (error) {
+            displayDiv.innerHTML = `<div class = 'main-body'>
+                                    <strong>Connection error, please try again</strong>
+                                    <div>`;
+        }
+    }
+}
+
+
+const staffModal = document.querySelector('.staff-modal');
+
+const showStaffModal = () => {
+    staffModal.style.display = 'block';
+}
+
+
+
+const closeStaffModal = () => {
+    staffModal.style.display = 'none';
+}
+
+const adminModal = document.querySelector('.admin-modal');
+
+const showAdminModal = () => {
+    adminModal.style.display = 'block';
+}
+
+const closeAdminModal = () => {
+    adminModal.style.display = 'none';
+}
+
+const closeStaffModalbutton = document.querySelector('.close-staff');
+const noStaffButton = document.getElementById('staff-no');
+const yesStaffButton = document.getElementById('staff-yes');
+
+const closeAdminModalbutton = document.querySelector('.close-admin');
+const noAdminButton = document.getElementById('admin-no');
+const yesAdminButton = document.getElementById('admin-yes');
+
+const makeUserAStaff = async () => {
+    staffModal.style.display = 'none';
+    displayDiv.innerHTML = `<img src = '../images/fading.gif'>`
+    if (!userToken) {
+        location.href = '../UI/pages/sign-in.html';
+    } else {
+        try {
+            const response = await fetch(staffUrl, { method: 'PATCH', headers: { 'Content-type': 'application/json',
+                                                                              'x-access-token': userToken  
+                                                                            }});
+            const jsonResponse = await response.json();
+            const userDetails = jsonResponse.data;    
+            if (jsonResponse.status === 200){
+                displayDiv.innerHTML = `<div class = 'main-body'>
+                                        <h2>Staff Rights: ${userDetails.isStaff}</h2>
+                                        <button id = 'staff' onclick = 'showStaffModal()'>Update Rights</button><br><br>
+                                        <h2>Admin Rights: ${userDetails.isAdmin}</h2>
+                                        <button id = 'admin' onclick = 'showAdminModal()'>Update Rights</button><br><br>
+                                        </div>
+                                        `;
+            } else if (jsonResponse.status === 404){
+                displayDiv.innerHTML = `<div class = 'main-body'><strong>${jsonResponse.error}</strong></div>`;
+            }  else if (jsonResponse.status === 403) {
+                location.href = '../pages/sign-in.html'
+            }                                                             
+        
+        } catch (error) {
+            displayDiv.innerHTML = `<div class = 'main-body'>
+                                    <strong>Connection error, please try again</strong>
+                                    <div>`;
+        }
+    }
+}
+
+const makeUserAnAdmin = async () => {
+    adminModal.style.display = 'none';
+    displayDiv.innerHTML = `<img src = '../images/fading.gif'>`
+    if (!userToken) {
+        location.href = '../UI/pages/sign-in.html';
+    } else {
+        try {
+            const response = await fetch(adminUrl, { method: 'PATCH', headers: { 'Content-type': 'application/json',
+                                                                              'x-access-token': userToken  
+                                                                            }});
+            const jsonResponse = await response.json();  
+            const userDetails = jsonResponse.data;  
+            if (jsonResponse.status === 200){
+                displayDiv.innerHTML = `<div class = 'main-body'>
+                                        <h2>Staff Rights: ${userDetails.isStaff}</h2>
+                                        <button id = 'staff' onclick = 'showStaffModal()'>Update Rights</button><br><br>
+                                        <h2>Admin Rights: ${userDetails.isAdmin}</h2>
+                                        <button id = 'admin' onclick = 'showAdminModal()'>Update Rights</button><br><br>
+                                        </div>
+                                        `;
+            } else if (jsonResponse.status === 404){
+                displayDiv.innerHTML = `<div class = 'main-body'><strong>${jsonResponse.error}</strong></div>`;
+            }  else if (jsonResponse.status === 403) {
+                location.href = '../pages/sign-in.html'
+            }                                                             
+        
+        } catch (error) {
+            displayDiv.innerHTML = `<div class = 'main-body'>
+                                    <strong>Connection error, please try again</strong>
+                                    <div>`;
+        }
+    }
+}
+
+
+closeStaffModalbutton.addEventListener('click', closeStaffModal);
+noStaffButton.addEventListener('click', closeStaffModal);
+yesStaffButton.addEventListener('click', makeUserAStaff);
+
+closeAdminModalbutton.addEventListener('click', closeAdminModal);
+noAdminButton.addEventListener('click', closeAdminModal);
+yesAdminButton.addEventListener('click', makeUserAnAdmin);
+
+
+
+
+
+
 
 const styleDetailsButton = () => {
     detailsButton.style.background = '#ddd';
@@ -76,6 +224,6 @@ getUserDetails();
 detailsButton.addEventListener('click', getUserDetails);
 detailsButton.addEventListener('click', styleDetailsButton);
 
-//manageButton.addEventListener('click', manageAccount);
+manageButton.addEventListener('click', manageUserAccount);
 manageButton.addEventListener('click', styleManageButton);
 
